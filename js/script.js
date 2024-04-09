4488bd911a178866ba0ecb65dcf339af2814ab98
iTsGiuse <giuse.amato@icloud.com>
2024-04-09 16:57:12 +0200

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ createApp({
             listEmail: [],
             email:'',
             nEmail: 9,
+            listEmailBonus: [],
+            emailBonus:'',
+            emailGenerate: false,
         };
     },
     methods: {
@@ -23,12 +26,32 @@ createApp({
             for(let i = 0; i<this.nEmail; i++){
             this.getEmailFromApi();
             }
-        }
+        },
 
+        getEmailFromApiBonus() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+            then((response) => {
+                const serverData = response.data;
+                this.emailBonus = serverData.response;
+                this.listEmailBonus.push(this.emailBonus);
+                if (this.listEmailBonus.length === this.nEmail) {
+                    this.emailGenerate = true; 
+                }
+            });
+        },
+
+        generaDieciMailBonus(nEmail) {
+            for (let i = 0; i < this.nEmail; i++) {
+                this.getEmailFromApiBonus();
+            }
+        }
     },
 
     mounted() {
         this.generaDieciMail();
         this.getEmailFromApi();
-    }
+        this.generaDieciMailBonus();
+        this.getEmailFromApiBonus();
+    },
+
 }).mount('#app');
