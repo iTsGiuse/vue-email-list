83fb60f2262ee6ecdeb55effcaa06c13c2c4def3
iTsGiuse <giuse.amato@icloud.com>
2024-04-09 16:07:41 +0200

    faet: create 'listEmail' per pushare le mail dentro l'array

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            listEmail: [],
             email:'',
         };
     },
     methods: {
+
         getEmailFromApi() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
             then((response) => {
                 const serverData = response.data;
                 this.email = serverData.response;
+                this.listEmail.push(this.email)
+                console.log(listEmail)
             });
         }
     },
