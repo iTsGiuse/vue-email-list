6bbc4b0f590d3e8f7e340bd3951a5b21b24da2cf
iTsGiuse <giuse.amato@icloud.com>
2024-04-09 15:30:36 +0200

    feat: add api "generator email" and generate 1 mail

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        
+        return {
+            email:'',
+        };
     },
     methods: {
-
-  },
-})
-
-.mount('#app');
+        getEmailFromApi() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+            then((response) => {
+                const serverData = response.data;
+                this.email = serverData.response;
+            });
+        }
+    },
+    mounted() {
+        this.getEmailFromApi();
+    }
+}).mount('#app');
